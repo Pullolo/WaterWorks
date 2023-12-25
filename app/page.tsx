e3877627729edd6936b8ebc82ecb4317fc41e5af
Pullo 2006 <pllwot89@gmail.com>
2023-12-25 18:55:26 +0100

    minor text and style fixes

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <header><h1 className="text-6xl">Wodociągi</h1><h6 style={{textAlign: 'center', marginTop: '20px'}}>by Mikołaj Szlosowski</h6></header>
+      <header><h1 className="text-6xl">Wodociągi</h1><h6 style={{textAlign: 'center', marginTop: '30px'}}>by</h6><h6 style={{textAlign: 'center', marginTop: '20px'}}>Mikołaj Szlosowski</h6></header>
             <Link href={"/admin"} ><div className={styles.login}>Login as Admin</div></Link>
     </main>
   )
